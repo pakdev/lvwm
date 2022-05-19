df6474e6bc476bb6631d9fa5b08c35e4b4eedc91
Peter Kurlak <pkurlak@gmail.com>
2022-05-19 00:09:22 -0500

    Find project windows

--- src/layout.py
+++ src/layout.py
@@ -1,18 +1,22 @@
 from __future__ import annotations
 
 from abc import ABC
+from typing import cast
 
 from kivy.core.window import Window
 
-from view import View
+from view import LabViewManager, View
 
 
 class Layout(ABC):
     def __init__(self) -> None:
         super().__init__()
 
-        self._views: list[View] = []
-        self._visible_views: list[int] = []
+        self._lv_manager = LabViewManager()
+        projects = self._lv_manager.get_projects()
+
+        self._views: list[View] = cast(list[View], projects)
+        self._visible_views: list[View] = []
 
     def on_mamimize(self, window: Window):
         pass
--- src/view.py
+++ src/view.py
@@ -1,17 +1,35 @@
 from __future__ import annotations
 
+from abc import ABC
+import re
+
 import ahkpy
 
 
-class VirtualInstrument:
+class View(ABC):
     pass
 
 
-class Project:
-    def get_vis(self) -> list[VirtualInstrument]:
+class VirtualInstrument(View):
+    pass
+
+
+class Project(View):
+    def __init__(self, window):
+        self._window = window
+        match = re.search("(.+?) - Project Explorer", window.title)
+        if match:
+            print(match.groups[1])
+            self._name = window.title
+
+    def get_views(self) -> list[View]:
         pass
 
 
 class LabViewManager:
     def get_projects(self) -> list[Project]:
-        pass
+        return [
+            Project(win)
+            for win in ahkpy.windows.filter(class_name="LVDChild")
+            if "Project Explorer" in win.title
+        ]
